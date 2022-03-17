fa5f48c35c63b715dd1035823abb7b48d7f74f5d
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-03-18 00:45:07 +0800

    Update rule.py

--- script/rule.py
+++ script/rule.py
@@ -7,7 +7,7 @@ result=list(set(ffo))
 result.sort()
 #ist=str(result)
 #ist=ist.encode('utf-8')
-fo.writelines(result)
-#for line in result:
-#    fo.write(line+'\n')
+#fo.writelines(result)
+for line in result:
+    fo.write(line+'\n')
 fo.close()
