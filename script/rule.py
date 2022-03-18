024adcc1ada82def996b36014d18ad1661d0b018
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-03-18 19:12:32 +0800

    Update rule.py

--- script/rule.py
+++ script/rule.py
@@ -3,8 +3,8 @@ import os
 result = []
 ffo = open("adblock+adguard.txt", "rb+")
 fo = open("adblock+adguard-test.txt", "wb+")
-fffo=str(ffo)
-result=list(set(fffo))
+#fffo=str(ffo)
+result=list(set(ffo))
 result.sort()
 #ist=str(result)
 #ist=ist.encode('utf-8')
