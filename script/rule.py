dcba1770611d3ca7f016ffff14b65a1259564587
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-03-22 20:09:50 +0800

    Update rule.py

--- script/rule.py
+++ script/rule.py
@@ -1,107 +1,12 @@
 # -*- coding:utf-8 -*-
 import os
 #去重开始
-print("去重中...")
-'''
-#初始化
-result = []
-resultb = []
-resultc = []
-resultd = []
-resulte = []
-resultf = []
-resultg = []
-resulth = []
-
-#读取文件
-ffo = open("tmp-adblock+adguard.txt")
-fo = open("tmp-adblock+adguard-test.txt", "w")
-ffob = open("tmp-adblock.txt")
-fob = open("tmp-adblock-test.txt", "w")
-ffoc = open("tmp-adguard.txt")
-foc = open("tmp-adguard-test.txt", "w")
-ffod = open("tmp-dns.txt")
-fod = open("tmp-dns-test.txt", "w")
-ffoe = open("tmp-hosts.txt")
-foe = open("tmp-hosts-test.txt", "w")
-ffof = open("tmp-ad-domains.txt")
-fof = open("tmp-ad-domains-test.txt", "w")
-ffog = open("tmp-allow.txt")
-fog = open("tmp-allow-test.txt", "w")
-ffoh = open("tmp-adguard-full.txt")
-foh = open("tmp-adguard-full-test.txt", "w")
-
-#开始处理规则
-result=list(set(ffo.readlines()))
-result.sort()
-fo.writelines(result)
-fo.close()
-ffo.close()
-os.remove("tmp-adblock+adguard.txt")
-os.rename("tmp-adblock+adguard-test.txt","tmp-adblock+adguard.txt")
-
-resultb=list(set(ffob.readlines()))
-resultb.sort()
-fob.writelines(resultb)
-fob.close()
-ffob.close()
-os.remove("tmp-adblock.txt")
-os.rename("tmp-adblock-test.txt","tmp-adblock.txt")
-
-resultc=list(set(ffoc.readlines()))
-resultc.sort()
-foc.writelines(resultc)
-foc.close()
-ffoc.close()
-os.remove("tmp-adguard.txt")
-os.rename("tmp-adguard-test.txt","tmp-adguard.txt")
-
-resultd=list(set(ffod.readlines()))
-resultd.sort()
-fod.writelines(resultd)
-fod.close()
-ffod.close()
-os.remove("tmp-dns.txt")
-os.rename("tmp-dns-test.txt","tmp-dns.txt")
-
-resulte=list(set(ffoe.readlines()))
-resulte.sort()
-foe.writelines(resulte)
-foe.close()
-ffoe.close()
-os.remove("tmp-hosts.txt")
-os.rename("tmp-hosts-test.txt","tmp-hosts.txt")
-
-resultf=list(set(ffof.readlines()))
-resultf.sort()
-fof.writelines(resultf)
-fof.close()
-ffof.close()
-os.remove("tmp-ad-domains.txt")
-os.rename("tmp-ad-domains-test.txt","tmp-ad-domains.txt")
-
-resultg=list(set(ffog.readlines()))
-resultg.sort()
-fog.writelines(resultg)
-fog.close()
-ffog.close()
-os.remove("tmp-allow.txt")
-os.rename("tmp-allow-test.txt","tmp-allow.txt")
-
-resulth=list(set(ffoh.readlines()))
-resulth.sort()
-foh.writelines(resulth)
-foh.close()
-ffoh.close()
-os.remove("tmp-adguard-full.txt")
-os.rename("tmp-adguard-full-test.txt","tmp-adguard-full.txt")
-'''
-
+print("规则去重中...")
 files= os.listdir() #得到文件夹下的所有文件名称
 result = []
 for file in files: #遍历文件夹
      if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开
-          print('开始去重'+(file))
+          #print('开始去重'+(file))
           f = open(file); #打开文件
           result=list(set(f.readlines()))
           result.sort()
@@ -111,9 +16,8 @@ for file in files: #遍历文件夹
           fo.close()
           os.remove(file)
           os.rename('test'+ (file),(file))
-          print((file) + '去重完成')
+          #print((file) + '去重完成')
           
-print("操作完成")
 #处理完毕
 
 print("去重完毕")
