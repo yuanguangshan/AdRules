27f196508af1f08e41b6a42d53370d252725230e
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-03-31 00:05:13 +0800

    一些速度优化

--- script/update-rules.sh
+++ script/update-rules.sh
@@ -107,25 +107,25 @@ done
 wait
 
 curl --connect-timeout 60 -s -o - https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanProgramAD.list \
- | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' > hosts999.txt
+ | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' > hosts999.txt &
 
 curl --connect-timeout 60 -s -o - https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanAD.list \
- | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' > hosts998.txt
-
+ | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' > hosts998.txt &
+wait
 echo '规则下载完成'
 
 # 添加空格
 file="$(ls|sort -u)"
 for i in $file; do
-  echo -e '\n' >> $i
+  echo -e '\n' >> $i &
 done
-
+wait
 # Pre Fix rules
 echo '处理规则中...'
 cat hosts*.txt | sort -n| sed '/^$/d' | grep -v -E "^((#.*)|(\s*))$" \
  | grep -v -E "^[0-9\.:]+\s+(ip6\-)?(localhost|loopback)$" \
  | sed s/127.0.0.1/0.0.0.0/g | sed s/::/0.0.0.0/g |grep '0.0.0.0' |grep -Ev '.0.0.0.0 ' | sort \
- | uniq >base-src-hosts.txt
+ | uniq >base-src-hosts.txt &
 
 cat base-src-hosts.txt | sed '/^$/d' | grep -Ev '#|\$|@|!|/|\\|\*'\
  | grep -v -E "^((#.*)|(\s*))$" \
@@ -133,19 +133,19 @@ cat base-src-hosts.txt | sed '/^$/d' | grep -Ev '#|\$|@|!|/|\\|\*'\
  | sed 's/127.0.0.1 //' | sed 's/0.0.0.0 //' \
  | sed "s/^/||&/g" |sed "s/$/&^/g"| sed '/^$/d' \
  | grep -v '^#' | grep -v 'local' \
- | sort -n | uniq | awk '!a[$0]++' > abp-hosts.txt  #Hosts规则转ABP规则
+ | sort -n | uniq | awk '!a[$0]++' > abp-hosts.txt & #Hosts规则转ABP规则
 
 cat allow-domains*.txt | sed '/^$/d' | grep -v '#' \
  | sed "s/^/@@||&/g" | sed "s/$/&^/g"  \
- | sort -n | uniq | awk '!a[$0]++' > pre-allow.txt  #将允许域名转换为ABP规则
+ | sort -n | uniq | awk '!a[$0]++' > pre-allow.txt & #将允许域名转换为ABP规则
 
 cat allow-domains0.txt | sed '/^$/d' | grep -v "#" \
  |sed "s/^/@@||&/g" | sed "s/$/&^/g" | sort -n \
- | uniq | awk '!a[$0]++' > pre-allow1.txt  #将允许域名转换为ABP规则
+ | uniq | awk '!a[$0]++' > pre-allow1.txt & #将允许域名转换为ABP规则
 
 cat *.txt | sed '/^$/d' \
  |grep -E "^\/[a-z]([a-z]|\.)*\.$" \
- |sort -u > l.txt
+ |sort -u > l.txt &
 
 
 # Start Merge and Duplicate Removal
@@ -153,19 +153,19 @@ cat *.txt | sed '/^$/d' \
 echo 开始合并
 cat .././mod/rules/adblock-rules.txt easylist*.txt \
  | grep -Ev "^((\!)|(\[)).*" | grep -v 'local.adguard.org' |grep -E -v "^[\.||]+[com]+[\^]$" \
- | sort -n | uniq >> tmp-adblock.txt #处理主规则
+ | sort -n | uniq >> tmp-adblock.txt & #处理主规则
 
 cat .././mod/rules/adblock-rules.txt *easylist*.txt full-adg*.txt \
  |grep -Ev "^((\!)|(\[)).*" | grep -v 'local.adguard.org' \
- | sort -u | sort -n | uniq | awk '!a[$0]++' > tmp-adblock+adguard.txt #处理Plus规则
+ | sort -u | sort -n | uniq | awk '!a[$0]++' > tmp-adblock+adguard.txt & #处理Plus规则
 
 cat adguard*.txt \
  |grep -Ev "^((\!)|(\[)).*" \
- | sort -n | uniq | awk '!a[$0]++' > tmp-adguard.txt #处理AdGuard的规则
+ | sort -n | uniq | awk '!a[$0]++' > tmp-adguard.txt & #处理AdGuard的规则
 
 cat full-adguard*.txt \
  |grep -Ev "^((\!)|(\[)).*" \
- | sort -n | uniq | awk '!a[$0]++' > tmp-adguard-full.txt #处理AdGuard的Full规则
+ | sort -n | uniq | awk '!a[$0]++' > tmp-adguard-full.txt & #处理AdGuard的Full规则
 
 #cat ubo-adguard*.txt | grep -v '.!' | grep -v '^!' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' | sort -n | uniq | awk '!a[$0]++' > tmp-adguard-ubo.txt #处理AdGuard的规则
 #cat ubo-full-adguard*.txt | grep -v '.!' | grep -v '^!' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' | sort -n | uniq | awk '!a[$0]++' > tmp-adguard-full-ubo.txt #处理AdGuard的规则
@@ -173,27 +173,27 @@ cat full-adguard*.txt \
 cat .././mod/rules/*-rules.txt dns*.txt *easylist*.txt full-adg*.txt abp-hosts*.txt \
  | grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^$" \
  | grep -Ev "([0-9]{1,3}.){3}[0-9]{1,3}" \
- | sort > ll.txt 
+ | sort > ll.txt &
 wait
 
 cat l*.txt abp-hosts*.txt \
  |grep -v '^!' | grep -E -v "^[\.||]+[com]+[\^]$" \
- |sort -n |uniq >> tmp-dns.txt  #处理DNS规则
+ |sort -n |uniq >> tmp-dns.txt & #处理DNS规则
 
 cat base-src-hosts.txt tmp-dns.txt \
  | sed '/^$/d' |grep '^||\|^[0-9]' | grep -v '\*'\
  | grep -v './'| grep -v '^\[' | grep -v '.!' \
  | grep -v '.\$'|grep -Ev "([0-9]{1,3}.){3}[0-9]{1,3}" \
  |sed 's/||/0.0.0.0 /' | sed 's/\^//' | grep -v "^|" \
- | sort -n | uniq > tmp-hosts.txt  #处理Hosts规则
+ | sort -n | uniq > tmp-hosts.txt & #处理Hosts规则
 
 cat tmp-hosts.txt \
  | sed 's/0.0.0.0 //' \
- | sort -n | uniq > tmp-ad-domains.txt #处理广告域名
+ | sort -n | uniq > tmp-ad-domains.txt & #处理广告域名
 
 cat *.txt | grep '^@' \
- | sort -n | uniq > tmp-allow.txt #允许清单处理
-
+ | sort -n | uniq > tmp-allow.txt & #允许清单处理
+wait
 
 echo 规则合并完成
 # Move to Pre Filter
@@ -210,26 +210,28 @@ python .././script/rule.py
 # Start Add title and date
 diffFile="$(ls|sort -u)"
 for i in $diffFile; do
- n=`cat $i | wc -l`
- echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间） " >> tpdate.txt
- new=$(echo "$i" |sed 's/tmp-//g')
- echo "! Total count: $n" > $i-tpdate.txt
- cat ./tpdate.txt ./$i-tpdate.txt ./$i > ./$new
- rm $i *tpdate.txt
+ n=`cat $i | wc -l` &
+ echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间） " >> tpdate.txt &
+ new=$(echo "$i" |sed 's/tmp-//g') &
+ echo "! Total count: $n" > $i-tpdate.txt &
+ cat ./tpdate.txt ./$i-tpdate.txt ./$i > ./$new &
+ rm $i *tpdate.txt &
 done
+wait
 echo '规则添加统计数据完成'
 # Add Title and MD5
 cd ../
 mkdir -p ./md5/
 diffFile="$(ls pre |sort -u)"
 for i in $diffFile; do
- titleName=$(echo "$i" |sed 's#.txt#-title.txt#')
- cat ./mod/title/$titleName ./pre/$i | awk '!a[$0]++'> ./$i
- sed -i '/^$/d' $i
- md5sum $i | sed "s/$i//" > ./md5/$i.md5
- perl ./script/addchecksum.pl ./$i
+ titleName=$(echo "$i" |sed 's#.txt#-title.txt#') &
+ cat ./mod/title/$titleName ./pre/$i | awk '!a[$0]++'> ./$i &
+ sed -i '/^$/d' $i &
+ md5sum $i | sed "s/$i//" > ./md5/$i.md5 &
+ perl ./script/addchecksum.pl ./$i &
  #echo "合并${i}的标题中"
 done
+wait
 echo '规则处理完成'
 
 rm -rf pre
